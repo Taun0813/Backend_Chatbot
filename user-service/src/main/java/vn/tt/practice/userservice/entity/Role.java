bcbf75c151464e0c8f68b1a56b5253692ae46fcd
TaunPro <yentuan130803@gmail.com>
2026-01-23 17:25:16 +0700

    update eureka

--- user-service/src/main/java/vn/tt/practice/userservice/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package vn.tt.practice.userservice.entity;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_SUPER_ADMIN
-}
--- user-service/src/main/java/vn/tt/practice/userservice/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package vn.tt.practice.userservice.model;
-
-public enum UserRole {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_SUPER_ADMIN
-}
